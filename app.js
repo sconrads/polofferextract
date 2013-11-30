2bbff9d3a1f7f0dcf4c7b9a41e68222313153ba4
Stian Conradsen <conradsen@gmail.com>
2013-11-30 19:48:47 +0100

    New loop fix

--- app.js
+++ app.js
@@ -31,26 +31,26 @@ polData.find({}, { stream: true })
             {
               price = attrValue;
             }
-
-            if ( (time.getMonth() === new Date().getMonth()) && (time.getYear() === new Date().getYear()) )
-            {
-              priceThisMonth = price;
-            }
-            else if ( ((time.getMonth() -1) === (new Date().getMonth() - 1)) && (time.getYear() === new Date().getYear()) && (new Date().getMonth() > 0) )
-            {
-              priceLastMonth = price;
-            }
-            // If today is january, compare with price last december last year
-            else if ( (new Date().getMonth() === 0) && (time.getMonth() === 11) && (time.getYear() === new Date().getYear() -1) )
-            {
-              priceLastMonth = price;
-            }
-            console.log("Price: " + price);
-            console.log("Time: " + time);    
+            
         }
-      console.log("Price this month: " + priceThisMonth);
-      console.log("Price last month: " + priceLastMonth);   
+        if ( (time.getMonth() === new Date().getMonth()) && (time.getYear() === new Date().getYear()) )
+        {
+          priceThisMonth = price;
+        }
+        else if ( ((time.getMonth() -1) === (new Date().getMonth() - 1)) && (time.getYear() === new Date().getYear()) && (new Date().getMonth() > 0) )
+        {
+          priceLastMonth = price;
+        }
+        // If today is january, compare with price last december last year
+        else if ( (new Date().getMonth() === 0) && (time.getMonth() === 11) && (time.getYear() === new Date().getYear() -1) )
+        {
+          priceLastMonth = price;
+        }
+        console.log("Price: " + price);
+        console.log("Time: " + time);
     }
+    console.log("Price this month: " + priceThisMonth);
+    console.log("Price last month: " + priceLastMonth);
   })
   .error(function(err){
     console.log(err);
